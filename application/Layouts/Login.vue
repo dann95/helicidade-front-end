82d07cde762b03a93177c0680a4a1d80d982ad9c
Daniel Rubin <danielrubin95@hotmail.com>
2018-08-16 19:05:41 -0300

    some work at login screen, goingo home, see u later <3

--- application/Layouts/Login.vue
+++ application/Layouts/Login.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        hue huehue login
+    <div id="login-container">
         <router-view></router-view>
     </div>
 </template>
@@ -12,5 +11,15 @@
 </script>
 
 <style scoped>
+    #login-container {
 
+        height: 100vh;
+        width: 100vw;
+
+        max-width: 100vw;
+        max-height: 100vh;
+
+        background: rgb(228,164,5) !important;
+        background: linear-gradient(90deg, rgba(228,164,5,1) 24%, rgba(239,193,3,1) 56%) !important;
+    }
 </style>
--- application/Views/Login.vue
+++ application/Views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="login-box">
         Login
     </div>
 </template>
@@ -11,5 +11,13 @@
 </script>
 
 <style scoped>
-
+    #login-box {
+        width: 759px;
+        height: 750px;
+        background-color:#fff;
+        margin:0 auto;
+        display:block;
+        box-shadow: -7.3px 10.8px 12.2px 0.8px rgba(84, 84, 84, 0.39);
+        border-radius: 5px;
+    }
 </style>
